317992c48231780ecb13ba1f357942cb4defc513
Stefan <stefan@sensler.de>
2021-10-15 21:30:47 +0200

    Konfiguration prettier + reformat + statische Status-Seite

---
+++ demo-ui/src/views/Status.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div>Status:</div>
+    <div class="status">{{ status }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup(props) {
+    console.log(props);
+    const status = 'UP';
+    return { props, status };
+  },
+};
+</script>
+
+<style>
+.status {
+  color: rebeccapurple;
+}
+</style>
--- demo-ui/vue.config.js
+++ demo-ui/vue.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-    devServer: {
-        proxy: {
-            '^/api': {
-                target: 'http://localhost:8080',
-                ws: true,
-                changeOrigin: true
-            },
-            '^/actuator': {
-                target: 'http://localhost:8080',    
-                ws: true,
-                changeOrigin: true
-            }
-        }
-    }
-}
+  devServer: {
+    proxy: {
+      '^/api': {
+        target: 'http://localhost:8080',
+        ws: true,
+        changeOrigin: true,
+      },
+      '^/actuator': {
+        target: 'http://localhost:8080',
+        ws: true,
+        changeOrigin: true,
+      },
+    },
+  },
+};
