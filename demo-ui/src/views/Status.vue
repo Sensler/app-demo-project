3e67649b075a7b291d62524b0d5472c677b9e898
Stefan <stefan@sensler.de>
2021-10-15 22:13:14 +0200

    Axios-Integration auf Status-Seite

--- demo-ui/src/views/Status.vue
+++ demo-ui/src/views/Status.vue
@@ -2,14 +2,33 @@
   <div>
     <div>Status:</div>
     <div class="status">{{ status }}</div>
+    <div>So oft wurde schon der Status abgerufen: {{ calls }}</div>
+    <button type="submit" @click="updateStatus" value="Status update" name="Status update">
+      Status update
+    </button>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue';
+import axios from 'axios';
+
 export default {
-  setup(props) {
-    const status = 'läuft';
-    return { props, status };
+  setup() {
+    const status = ref('läuft');
+    const calls = ref(0);
+
+    function updateStatus() {
+      axios
+        .get('/actuator/health')
+        .then((response) => {
+          status.value = response.data.status;
+        })
+
+        .catch((e) => (status.value = `Pfeife, Server läuft nicht: ${e}`))
+        .finally(() => (calls.value += 1));
+    }
+    return { status, calls, updateStatus };
   },
 };
 </script>
